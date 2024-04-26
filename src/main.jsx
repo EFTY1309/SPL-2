8b9c18a9d41d61addc613786ae0499c03d09077e
Rafid13iit <rafidssrr.6767@gmail.com>
2024-04-26 15:58:03 +0600

    Initially Footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './App.css'; 
-import App from "./App.jsx"
+import App from "./App.jsx";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
